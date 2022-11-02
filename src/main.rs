dc85c7a7c90b38ef12a250c41be7c241cc7f6d0c
Davis Silverman <sinistersnare@gmail.com>
2022-11-02 11:03:13 -0400

    Example showing not-working code w/ race

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,40 @@
     windows_subsystem = "windows"
 )]
 
-pub fn main() {
+#[allow(dead_code)]
+fn not_working<A: tauri::Assets>(ctx: tauri::Context<A>) -> Result<(), tauri::Error> {
     let mut app = tauri::Builder::default()
-        .build(tauri::generate_context!())
+        .build(ctx)
         .expect("Error building Tauri app.");
+    tauri::WindowBuilder::new(&app, "main", tauri::WindowUrl::App("index.html".into())).build()?;
     loop {
         let iteration = app.run_iteration();
         if iteration.window_count == 0 {
             break;
         }
     }
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn working<A: tauri::Assets>(ctx: tauri::Context<A>) -> Result<(), tauri::Error> {
+    let mut app = tauri::Builder::default()
+        .build(ctx)
+        .expect("Error building Tauri app.");
+    app.run_iteration(); // <<------- KEY LINE TO AVOID RACE
+    tauri::WindowBuilder::new(&app, "main", tauri::WindowUrl::App("index.html".into())).build()?;
+    loop {
+        let iteration = app.run_iteration();
+        if iteration.window_count == 0 {
+            break;
+        }
+    }
+    Ok(())
+}
+
+pub fn main() -> Result<(), tauri::Error> {
+    let ctx = tauri::generate_context!();
+    // not_working(ctx)?;
+    working(ctx)?;
+    Ok(())
 }
